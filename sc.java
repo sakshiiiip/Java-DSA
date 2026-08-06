7fe057a0665e8c27f24a54be50e1c1ac4b3d9193
sakshi <pandeysakshii277@gmail.com>
2026-08-06 13:54:53 +0530

    Palindrome

---
+++ sc.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+class sc {
+
+    public static int nextInt() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'nextInt'");
+    }
+
+}
